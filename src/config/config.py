925850f82e22d8423cc22ea0eb736159b0c67d7c
lguerra98 <guerra0815@gmail.com>
2025-04-04 19:33:16 -0500

    arreglos paths

--- src/config/config.py
+++ src/config/config.py
@@ -3,4 +3,4 @@ import os
 path = os.path.split(os.getcwd())[0]
 
 INPUT_PATH = f"{path}/data/Films_2.xlsx"
-DB_PATH = f"sqlite:///{path}/datadatabase.db"
+DB_PATH = f"sqlite:///{path}/data/database.db"
